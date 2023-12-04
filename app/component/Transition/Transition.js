fed85b562318ba73115d9b2d3d746c74558676ba
‪Ehab Yousef‬‏ <74505597+ehabyousef@users.noreply.github.com>
2023-12-04 19:45:05 +0200

    edit style

--- app/component/Transition/Transition.js
+++ app/component/Transition/Transition.js
@@ -9,18 +9,21 @@ const Transition = () => {
                 animate={{ y: '0%', height: '0%' }}
                 transition={{ duration: '1', ease: "easeInOut" }}
                 className={style.all_2}
+                id={style.state}
             />
             <motion.div
                 initial={{ y: "100%", height: '100%' }}
                 animate={{ y: '0%', height: '0%' }}
                 transition={{ delay: '0.2', duration: '1', ease: "easeInOut" }}
                 className={style.all_3}
+                id={style.state}
             />
             <motion.div
                 initial={{ y: "100%", height: '100%' }}
                 animate={{ y: '0%', height: '0%' }}
                 transition={{ delay: '.4', duration: '1', ease: "easeInOut" }}
                 className={style.all_4}
+                id={style.state}
             />
         </div>
     );
